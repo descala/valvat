a2b37043f5f27f69984c7e0298055a144e7ceee6
Lluís Gili <lluis@ingent.net>
2019-07-22 13:50:34 +0200

    restore default message for spec

--- spec/valvat/checksum/ie_spec.rb
+++ spec/valvat/checksum/ie_spec.rb
@@ -4,7 +4,7 @@ describe Valvat::Checksum::IE do
   %w(IE8473625E IE0123459N IE9B12345N IE1113571MH IE2973912UH IE2974611LH
   IE2974901UH IE3200115LH IE3206791MH IE3208913KH IE3214048CH).each do |valid_vat|
     it "returns true on valid vat #{valid_vat}" do
-      expect(Valvat::Checksum.validate(valid_vat)).to eql(true), valid_vat
+      expect(Valvat::Checksum.validate(valid_vat)).to eql(true)
     end
 
     invalid_vat = "#{valid_vat[0..-4]}#{valid_vat[-2]}#{valid_vat[-3]}#{valid_vat[-1]}"
